e9eb483059bff67d4979d3c8322e8393c300dd6e
mukul <mukulbhardwaj966@gmail.com>
2024-03-31 20:37:44 +0530

    added some comments

---
+++ src/Components/SampleData.jsx
@@ -0,0 +1,70 @@
+
+const SampleData = [
+    [
+        {
+            "assignees": "dev4",
+            "description": "Developed RESTful APIs with Node.js and Express for a social media platform. Utilized NoSQL database for scalable data storage and retrieval. Implemented real-time messaging features using WebSockets. Designed and optimized backend infrastructure for high performance and scalability.",
+            "priority": "P0",
+            "status": "Assigned",
+            "team": "mukul",
+            "title": "Social Media API"
+        },
+        {
+            "assignees": "dev5",
+            "description": "Built a microservices architecture using Node.js and Express to handle various API functionalities. Utilized SQL database for transactional data and NoSQL database for real-time analytics. Implemented caching strategies for performance optimization and improved user experience.",
+            "priority": "P1",
+            "status": "Assigned",
+            "team": "mukul",
+            "title": "Microservices Architecture"
+        }
+    ],
+    [
+        {
+            "assignees": "dev3",
+            "description": "Designed and implemented a dashboard application using React and Redux for state management. Integrated backend APIs for data retrieval and manipulation. Implemented responsive UI components and data visualization for better user interaction.",
+            "priority": "P0",
+            "status": "In Progress",
+            "team": "mukul",
+            "title": "Dashboard Application"
+        }
+    ],
+    [
+        {
+            "assignees": "dev9",
+            "description": "Developed a note-taking web application using React and Express. Implemented features such as Note bookmarking, CRUD operations via RESTful API with MongoDB database integration. Utilized Axios for making asynchronous API calls from the frontend. Implemented user authentication and authorization mechanisms to ensure secure access to user-specific notes.",
+            "priority": "P1",
+            "status": "Completed",
+            "team": "mukul",
+            "title": "Note-Taking Web App"
+        },
+        {
+            "assignees": "dev2",
+            "description": "Developed a mini e-commerce application using WinForms and Visual Studio, and PostgreSQL. Implemented user authentication, user and farmer dashboards, order management, inventory tracking, and sales analytics for comprehensive business operations.",
+            "priority": "P2",
+            "status": "Completed",
+            "team": "mukul",
+            "title": "Mini E-commerce App"
+        }
+    ],
+    [
+        {
+            "assignees": "dev7",
+            "description": "Designed and implemented backend services using Express.js and PostgreSQL for managing user accounts and data processing. Integrated React components for user interfaces and state management with Redux. Implemented RESTful API endpoints for data retrieval and manipulation to ensure smooth application functionality.",
+            "priority": "P1",
+            "status": "Deployed",
+            "team": "mukul",
+            "title": "Backend Services Development"
+        }
+    ],
+    [
+        {
+            "assignees": "dev1",
+            "description": "Built RESTful APIs with Express.js and MongoDB for a blogging platform. Designed database schemas for user accounts and blog posts. Integrated React components for frontend views and Redux for state management. Ensured efficient data handling and seamless user experience.",
+            "priority": "P2",
+            "status": "Deferred",
+            "team": "mukul",
+            "title": "Blogging Platform APIs"
+        }
+    ]
+];
+export default SampleData;
